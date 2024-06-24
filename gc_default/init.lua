3e888486083e70ccc0e8f33606e79b2fcbf09d0f
orion-tux <alex.sejnost@pm.me>
2024-06-24 21:08:44 +0200

    edits

--- gc_default/init.lua
+++ gc_default/init.lua
@@ -1,2 +1,3 @@
 dofile(minetest.get_modpath("gc_default") .. "/nodes.lua")
 dofile(minetest.get_modpath("gc_default") .. "/mapgen.lua")
+dofile(minetest.get_modpath("gc_default") .. "/mechanics.lua")
