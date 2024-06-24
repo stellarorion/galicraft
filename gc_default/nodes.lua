02d4514ff1d8110c2307c5764bfea9268215f643
orion-tux <alex.sejnost@pm.me>
2024-06-24 14:14:14 +0200

    Registered water

--- gc_default/nodes.lua
+++ gc_default/nodes.lua
@@ -44,3 +44,92 @@ minetest.register_node("gc_default:grass_block", {
         soil = 1
     }
 })
+
+-- Liquid things
+
+minetest.register_node("gc_default:water_source", {
+	description = ("Water Source"),
+	drawtype = "liquid",
+	waving = 3,
+	tiles = {
+		{
+			name = "gc_water_source_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 3,
+			},
+		},
+		{
+			name = "gc_water_source_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 3,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "gc_default:water_flowing",
+	liquid_alternative_source = "gc_default:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, cools_lava = 1},
+})
+
+minetest.register_node("gc_default:water_flowing", {
+	description = ("Flowing Water"),
+	drawtype = "flowingliquid",
+	waving = 3,
+	tiles = {"gc_water.png"},
+	special_tiles = {
+		{
+			name = "gc_water_flowing_animated.png",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2,
+			},
+		},
+		{
+			name = "gc_water_flowing_animated.png",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2,
+			},
+		},
+	},
+	use_texture_alpha = "blend",
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "gc_default:water_flowing",
+	liquid_alternative_source = "gc_default:water_source",
+	liquid_viscosity = 1,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1},
+})
