66d43f114b72d110b41b3446c5d4cb87118d3272
orion-tux <alex.sejnost@pm.me>
2024-06-25 19:01:30 +0200

    small tweaks to biome generation

--- mods/gc_default/mapgen.lua
+++ mods/gc_default/mapgen.lua
@@ -6,11 +6,11 @@ minetest.register_alias("mapgen_river_water_source", "gc_default:water_source")
 minetest.register_biome({
     name = "grasslands1",
     node_top = "gc_default:grass_block",
-    depth_top = 10,
+    depth_top = 1,
     node_filler = "gc_default:dirt",
-    depth_filler = 200,
-    y_max = 1000,
+    depth_filler = 60,
+    y_max = 500,
     y_min = -3,
-    heat_point = 50,
-    humidity_point = 50,
+    heat_point = 10,
+    humidity_point = 10,
 })
