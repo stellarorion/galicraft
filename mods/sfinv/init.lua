a3b3629adf596a9479cc89234e2c027d3725e229
orion-tux <alex.sejnost@pm.me>
2024-06-25 18:57:40 +0200

    I tried replacing the weird blobs, but it just colors the hotbar for some reason. Anyway, added my own arrow in the craft menu.

--- mods/sfinv/init.lua
+++ mods/sfinv/init.lua
@@ -11,7 +11,7 @@ sfinv.register_page("sfinv:crafting", {
 		return sfinv.make_formspec(player, context, [[
 				list[current_player;craft;1.75,0.5;3,3;]
 				list[current_player;craftpreview;5.75,1.5;1,1;]
-				image[4.75,1.5;1,1;sfinv_crafting_arrow.png]
+				image[4.75,1.5;1,1;gc_inventory_craft_arrow.png]
 				listring[current_player;main]
 				listring[current_player;craft]
 			]], true)
