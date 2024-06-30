0bd31dc5d2d58c28ce4ec24d206e38dbf7b48edf
orion-tux <alex.sejnost@pm.me>
2024-06-30 20:50:18 +0200

    I don't wanna be dependent on anything

--- mods/gc_default/init.lua
+++
@@ -1,3 +0,0 @@
-dofile(minetest.get_modpath("gc_default") .. "/nodes.lua")
-dofile(minetest.get_modpath("gc_default") .. "/mapgen.lua")
-dofile(minetest.get_modpath("gc_default") .. "/mechanics.lua")
