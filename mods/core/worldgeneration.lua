44c711dff7d513fa144ed6342bba363a8a7c64df
orion-tux <alex.sejnost@pm.me>
2024-07-01 13:46:12 +0200

    Registered basic world generation

---
+++ mods/core/worldgeneration.lua
@@ -0,0 +1,5 @@
+-- Define bare minimum blocks for the world generation
+minetest.register_alias("mapgen_stone", "core:stone")
+minetest.register_alias("mapgen_water_source", "core:water_source")
+minetest.register_alias("mapgen_lava_source", "core:lava_source")
+
