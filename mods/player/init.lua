a5fc0078bd3f108f6595231e6058ef52a82e53ba
orion-tux <alex.sejnost@pm.me>
2024-07-02 20:55:13 +0200

    + tools mod

--- mods/player/init.lua
+++ mods/player/init.lua
@@ -9,6 +9,24 @@ local function set_speed(player)
     end
 end
 
+minetest.register_tool(":", {
+    description = "My Tool",
+    inventory_image = "wieldhand.png",
+    tool_capabilities = {
+        full_punch_interval = 1.5,
+        max_drop_level = 1,
+        groupcaps = {
+            crumbly = {
+                maxlevel = 2,
+                uses = 20,
+                times = { [1]=1.60, [2]=1.20, [3]=0.80 }
+            },
+        },
+        damage_groups = {fleshy=2},
+    },
+})
+
+
 minetest.register_globalstep(function(dtime)
     for _, player in ipairs(minetest.get_connected_players()) do
         set_speed(player)
