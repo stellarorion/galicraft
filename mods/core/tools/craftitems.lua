b7bc8973a3fa066a1e5e1b9fcac6f2f270bfc659
orion-tux <alex.sejnost@pm.me>
2024-07-02 21:00:20 +0200

    Talk to the hand

---
+++ mods/core/tools/craftitems.lua
@@ -0,0 +1,16 @@
+minetest.register_tool(":", {
+    description = "Talk to the hand",
+    inventory_image = "wieldhand.png",
+    tool_capabilities = {
+        full_punch_interval = 1.5,
+        max_drop_level = 1,
+        groupcaps = {
+            crumbly = {
+                maxlevel = 2,
+                uses = 20,
+                times = { [1]=1.60, [2]=1.20, [3]=0.80 }
+            },
+        },
+        damage_groups = {fleshy=2},
+    },
+})
---
+++ mods/core/tools/init.lua
@@ -0,0 +1,1 @@
+dofile(minetest.get_modpath("tools") .. "/craftitems.lua")
